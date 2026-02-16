85b6e49e00ef9cf7e2a395f3fd026ab920d05748
maliksaadsajjad <mr.aantojdz@gmail.com>
2026-02-16 16:38:55 +0500

    Validating user input on every key stroke

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -8,6 +8,9 @@ export default function Login() {
     password: '',
   });
 
+  const emailIsInvalid =
+    enteredvalues.email !== '' && !enteredvalues.email.includes('@');
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -43,6 +46,9 @@ export default function Login() {
             onChange={(event) => handleInputChange('email', event.target.value)}
             value={enteredvalues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
